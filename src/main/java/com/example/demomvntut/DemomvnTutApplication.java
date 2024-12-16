fdc2667c6db0b859451d386811a75f7e5fb54f04
Aidan <aidanfire360@gmail.com>
2024-12-16 17:20:54 -0500

    first commit

---
+++ src/main/java/com/example/demomvntut/DemomvnTutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomvntut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomvnTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemomvnTutApplication.class, args);
+    }
+
+}
